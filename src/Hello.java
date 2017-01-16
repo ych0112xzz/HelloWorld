13853bbf3b2c55294a8c5e6b8f66fe879681e448
ych0112xzz <1149851790@qq.com>
2017-01-16 16:48:23 +0800

    test

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ych0112xzz on 2017/1/16.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // write your code here
+    }
+}
